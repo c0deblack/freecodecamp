9a8bb96b0ee07c148771a45031d05d6f15e2631c
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-18 07:24:44 -0400

    Confirm the ending.

---
+++ js/basic-algorithm-scripting/confirm-the-ending.js
@@ -0,0 +1,41 @@
+/*
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  return str.slice(str.length - target.length) === target;
+}
+*/
+
+/*
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  let re = new RegExp(target + "$", "i");
+
+  return re.test(str);
+}
+
+*/
+
+/*
+function confirmEnding(str, target) {
+  return str.slice(-target.length) === target
+}
+
+*/
+
+
+function confirmEnding(str, target) {
+  const targetLen = target.length;
+  const strLen = str.length;
+
+  if (str.slice(str.length - target.length) == target) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(confirmEnding("Congratulation", "on"));
