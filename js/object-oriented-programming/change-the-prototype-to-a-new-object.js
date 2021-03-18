41b81c7c8482cc642999fa8ddb10de8952575a53
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-18 07:53:13 -0400

    Change the prototype to a new object.

---
+++ js/object-oriented-programming/change-the-prototype-to-a-new-object.js
@@ -0,0 +1,20 @@
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+
+  numLegs: 4,
+  eat: () => {
+    console.log("eat");
+  },
+  describe: () => {
+    console.log("describe");
+  }
+};
+
+console.log(Dog.hasOwnProperty("name"));
+let tt = new Dog();
+console.log(tt.hasOwnProperty("name"));
+tt.eat();
+tt.describe();
