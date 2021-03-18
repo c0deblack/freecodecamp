9ba14460815617eb01739d559f7b0b3445e60fac
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-18 07:25:29 -0400

    Title case a sentence.

---
+++ js/basic-algorithm-scripting/title-case-a-sentence.js
@@ -0,0 +1,48 @@
+/*
+String.prototype.replaceAt = function(index, character) {
+  return (
+    this.substr(0, index) + character + this.substr(index + character.length)
+  );
+};
+
+function titleCase(str) {
+  var newTitle = str.split(" ");
+  var updatedTitle = [];
+  for (var st in newTitle) {
+    updatedTitle[st] = newTitle[st]
+      .toLowerCase()
+      .replaceAt(0, newTitle[st].charAt(0).toUpperCase());
+  }
+  return updatedTitle.join(" ");
+}
+*/
+
+/*
+function titleCase(str) {
+  var convertToArray = str.toLowerCase().split(" ");
+  var result = convertToArray.map(function(val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
+}
+
+*/
+/*
+function titleCase(str) {
+  return str.toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase());
+}
+*/
+
+
+function titleCase(str) {
+  const retStr = [];
+  const words = str.split(" ");
+
+  //console.log(words);
+  for (let i = 0; i < words.length; i++) {
+    retStr.push(words[i][0].toUpperCase() + words[i].slice(1).toLowerCase());
+  }
+  return retStr.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
