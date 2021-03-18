00c26d3864fd7c790680b0fda45daf77bd342198
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-18 07:52:40 -0400

    Define a constructor function.

---
+++ js/object-oriented-programming/define-a-constructor-function.js
@@ -0,0 +1,12 @@
+/*
+function Dog() {
+  (this.name = "Geogre"), (this.color = "White"), (this.numLegs = 4);
+}
+
+*/
+
+function Dog() {
+    this.name = "Fifo";
+    this.color = "brown";
+    this.numLegs = 4;
+}
