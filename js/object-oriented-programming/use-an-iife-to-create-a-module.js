0219d8bd6aab79548aab5e54ddbfcb3d353c2ee2
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-18 07:54:07 -0400

    Use an iife to create a module.

---
+++ js/object-oriented-programming/use-an-iife-to-create-a-module.js
@@ -0,0 +1,29 @@
+
+/*
+let funModule = ( () => {
+  return {
+    isCuteMixin: (obj) => {
+      obj.isCute = () => { true; };
+    },
+    singMixin: (obj) => {
+      obj.sing = () => { console.log("Singing to an awesome tune"); }
+    }
+
+  }
+})();
+*/
+
+const funModule = (function (){
+  return {
+    isCuteMixin: function(obj) {
+      obj.isCute = function() {
+        return true;
+      };
+    },
+    singMixin: function(obj) {
+      obj.sing = function() {
+        console.log("Singing to an awesome tune");
+      };
+    }
+  }
+})();
