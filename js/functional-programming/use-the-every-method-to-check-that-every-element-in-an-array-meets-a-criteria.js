bbf473fdb653bebf9ae592ff1ba4d0378668dcea
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-18 10:34:05 -0400

    Use the every method to check that every element in an array meets a criteria.

---
+++ js/functional-programming/use-the-every-method-to-check-that-every-element-in-an-array-meets-a-criteria.js
@@ -0,0 +1,14 @@
+/*
+function checkPositive(arr) {
+
+  return arr.every(function(value) {
+    return value > 0;
+  });
+
+}
+*/
+
+function checkPositive(arr) {
+  return arr.every(val => val >= 0);
+}
+checkPositive([1, 2, 3, -4, 5]);
