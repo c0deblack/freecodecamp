bbc8012632e470b8ab9f5fed89d647ff92d189ed
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-22 01:26:43 -0400

    Truncate a string.

---
+++ javascript-algorithms-and-data-structures-certification/basic-algorithm-scripting/truncate-a-string.js
@@ -0,0 +1,24 @@
+/*
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+*/
+/*
+function truncateString(str, num) {
+  return str.length > num ? str.slice(0, num) + "..." : str;
+}
+*/
+
+function truncateString(str, num) {
+  if (str.length > num ) {
+    const re = new RegExp("^.{"+num+"}");
+    return str.match(re)[0] + "...";
+  }
+  return str;
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
