cbec1d0e0d0f9249c7d9818f4ad57419111dc340
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-03-22 01:28:08 -0400

    Apply functional programming to convert strings to url slugs.

---
+++ javascript-algorithms-and-data-structures-certification/functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -0,0 +1,30 @@
+/*
+function urlSlug(title) {
+  return title
+    .split(" ")
+    .filter(substr => substr !== "")
+    .join("-")
+    .toLowerCase();
+}
+*/
+
+/*
+function urlSlug(title) {
+  return title
+    .toLowerCase()
+    .trim()
+    .split(/\s+/)
+    .join("-");
+}
+*/
+
+function urlSlug(title) {
+  return title
+  .toLowerCase()
+  .split(" ")
+  .filter(word => word.length > 0)
+  .join("-");
+}
+
+
+console.log(urlSlug(" Winter Is  Coming"));
