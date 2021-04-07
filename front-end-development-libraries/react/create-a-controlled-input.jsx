a06cbda8a7be0bfc889d3818dc9f940218c2cc2c
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-07 17:04:59 -0400

    Create a controlled input.

---
+++ front-end-development-libraries/react/create-a-controlled-input.jsx
@@ -0,0 +1,44 @@
+/*
+Create a Controlled Input
+
+With React the user's input becomes part of the application state.
+
+React controls the value of that input field.
+*/
+class ControlledInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: ''
+      };
+      
+      
+      this.handleChange = this.handleChange.bind(this);
+
+    }
+    
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
+    render() {
+      return (
+        <div>
+          <input onChange={this.handleChange} value={this.state.input} />
+          <h4>Controlled Input:</h4>
+          <p>{this.state.input}</p>
+        </div>
+      );
+    }
+  };
+/*
+First, create a method called handleChange() that has a parameter called event. When the method is called, it receives an event object that contains a string of text from the input element. You can access this string with event.target.value inside the method. Update the input property of the component's state with this new string.
+
+In the render method, create the input element above the h4 tag. Add a value attribute which is equal to the input property of the component's state. Then add an onChange() event handler set to the handleChange() method.
+
+When you type in the input box, that text is processed by the handleChange() method, set as the input property in the local state, and rendered as the value in the input box on the page. The component state is the single source of truth regarding the input data.
+
+Last but not least, don't forget to add the necessary bindings in the constructor.
+*/
