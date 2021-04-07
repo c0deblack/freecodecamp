8b39bf9eccaeda6bf60e834c0211f738e4bd384a
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-07 17:04:59 -0400

    Learn about self closing jsx tags.

---
+++ front-end-development-libraries/react/learn-about-self-closing-jsx-tags.jsx
@@ -0,0 +1,16 @@
+/*
+The line-break tag must always be written as <br />
+
+A <div>, on the other hand, can be written as <div /> or <div></div>.
+
+The difference is that in the first syntax version there is no way to include anything in the <div />.
+
+This syntax is useful when rendering React components.
+*/
+const JSX = (
+  <div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+  </div>
+);
