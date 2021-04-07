425da2a511371df7237b766bdbdae21e5b7c088b
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-07 17:04:59 -0400

    Add comments in jsx.

---
+++ front-end-development-libraries/react/add-comments-in-jsx.jsx
@@ -0,0 +1,11 @@
+{/*
+To put comments inside JSX, you use the syntax {} to wrap around the comment text.
+*/}
+
+const JSX = (
+  <div>
+    {/* This is a JSX Comment */}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+  </div>
+);
