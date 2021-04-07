902a6b96c1424ef7ea500c9cafc3a5db38eb74e9
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-07 17:04:59 -0400

    Create a component with composition.

---
+++ front-end-development-libraries/react/create-a-component-with-composition.jsx
@@ -0,0 +1,37 @@
+/*
+Create a Component with Composition
+
+Include the component name written as a custom HTML tag in the JSX
+
+return (
+ <App>
+  <Navbar />
+  <Dashboard />
+  <Footer />
+ </App>
+)
+*/
+const ChildComponent = () => {
+    return (
+      <div>
+            <p>I am the child</p>
+      </div>
+    );
+  };
+  
+class ParentComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+        <div>
+            <h1>I am the parent</h1>
+            <ChildComponent />
+        </div>
+        );
+    }
+};
+/*
+In the code editor, there is a simple functional component called ChildComponent and a class component called ParentComponent. Compose the two together by rendering the ChildComponent within the ParentComponent.
+*/
