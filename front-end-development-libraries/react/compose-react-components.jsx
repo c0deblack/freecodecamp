a4c2ab8878f1708df2ec44902c74c684f5ddf2b8
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-07 17:04:59 -0400

    Compose react components.

---
+++ front-end-development-libraries/react/compose-react-components.jsx
@@ -0,0 +1,41 @@
+/*
+Compose React Components
+
+You can render JSX elements, stateless functional components, and ES6 class components within other components.
+*/
+class Fruits extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h2>Fruits:</h2>
+
+          <NonCitrus />
+          <Citrus />
+
+        </div>
+      );
+    }
+  };
+  
+  class TypesOfFood extends React.Component {
+    constructor(props) {
+       super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+
+          <Fruits />
+
+          <Vegetables />
+        </div>
+      );
+    }
+  };
+/*
+Nest two components inside of Fruits — first NonCitrus, and then Citrus. Both of these components are provided for you behind the scenes. Next, nest the Fruits class component into the TypesOfFood component, below the h1 header and above Vegetables.
+*/
