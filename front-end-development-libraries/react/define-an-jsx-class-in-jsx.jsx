f2a34945d92c08b56aa05767e911297909ff7195
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-07 17:04:59 -0400

    Define an jsx class in jsx.

---
+++ front-end-development-libraries/react/define-an-jsx-class-in-jsx.jsx
@@ -0,0 +1,10 @@
+/*
+JSX uses className
+
+The naming convention for all HTML attributes and event references in JSX become camelCase
+*/
+ const JSX = (
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
+  </div>
+);
