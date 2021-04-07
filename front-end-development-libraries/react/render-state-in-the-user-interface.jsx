04ac22b0706d6c21942e18ae898e471195cbb60f
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-07 17:05:00 -0400

    Render state in the user interface.

---
+++ front-end-development-libraries/react/render-state-in-the-user-interface.jsx
@@ -0,0 +1,27 @@
+/*
+Render State in the User Interface
+
+It allows you to track important data in your app and render a UI in response to changes in this data.
+
+If your data changes, your UI will change.
+*/
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'freeCodeCamp'
+      }
+    }
+    render() {
+      return (
+        <div>
+          
+          <h1>{this.state.name}</h1>
+
+        </div>
+      )
+    }
+  }
+/*
+In the code editor, MyComponent is already stateful. Define an h1 tag in the component's render method which renders the value of name from the component's state.
+*/
