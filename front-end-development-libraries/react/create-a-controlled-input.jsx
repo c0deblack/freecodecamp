ab685f27675446d467a61585f49305aace183607
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-12 18:30:54 -0400

    Create a controlled input.

--- front-end-development-libraries/react/create-a-controlled-input.jsx
+++ front-end-development-libraries/react/create-a-controlled-input.jsx
@@ -4,6 +4,10 @@ Create a Controlled Input
 With React the user's input becomes part of the application state.
 
 React controls the value of that input field.
+
+When event methods are called, they receive an event object that contains a string of text from the input element.
+
+You can access this string with event.target.value inside the method.
 */
 class ControlledInput extends React.Component {
     constructor(props) {
@@ -34,11 +38,15 @@ class ControlledInput extends React.Component {
     }
   };
 /*
-First, create a method called handleChange() that has a parameter called event. When the method is called, it receives an event object that contains a string of text from the input element. You can access this string with event.target.value inside the method. Update the input property of the component's state with this new string.
+First, create a method called handleChange() that has a parameter called event.
 
-In the render method, create the input element above the h4 tag. Add a value attribute which is equal to the input property of the component's state. Then add an onChange() event handler set to the handleChange() method.
+Update the input property of the component's state with the event objects value.
 
-When you type in the input box, that text is processed by the handleChange() method, set as the input property in the local state, and rendered as the value in the input box on the page. The component state is the single source of truth regarding the input data.
+In the render method, create the input element above the h4 tag.
 
-Last but not least, don't forget to add the necessary bindings in the constructor.
+Add a value attribute which is equal to the input property of the component's state.
+
+Then add an onChange() event handler set to the handleChange() method.
+
+Add the necessary bindings in the constructor.
 */
