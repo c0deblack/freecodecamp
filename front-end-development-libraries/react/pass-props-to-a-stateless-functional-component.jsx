04eeed311f3d7e52672c114a68dd25829c5aedd8
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-12 18:30:54 -0400

    Pass props to a stateless functional component.

--- front-end-development-libraries/react/pass-props-to-a-stateless-functional-component.jsx
+++ front-end-development-libraries/react/pass-props-to-a-stateless-functional-component.jsx
@@ -1,10 +1,6 @@
 /*
 Pass Props to a Stateless Functional Component
 
-The previous challenges covered a lot about creating and composing JSX elements, functional components, and ES6 style class components in React. 
-
-With this foundation, it's time to look at another feature very common in React: props. 
-
 In React, you can pass props, or properties, to child components. 
 
 <App>
@@ -15,9 +11,11 @@ You use custom HTML attributes.
 
 Since Welcome is a stateless functional component, it has access to this value like so:
 
-const Welcome = (props) => <h1>Hello, {props.user}!</h1>
+  const Welcome = (props) => <h1>Hello, {props.user}!</h1>
 
 With class components this is a little different.
+
+Note that prop values must be enclosed in curly brackets, for instance date={Date()}.
 */
 const CurrentDate = (props) => {
     return (
@@ -45,5 +43,9 @@ const CurrentDate = (props) => {
     }
   };
 /*
-There are Calendar and CurrentDate components in the code editor. When rendering CurrentDate from the Calendar component, pass in a property of date assigned to the current date from JavaScript's Date object. Then access this prop in the CurrentDate component, showing its value within the p tags. Note that for prop values to be evaluated as JavaScript, they must be enclosed in curly brackets, for instance date={Date()}.
+When rendering CurrentDate from the Calendar component:
+
+  pass in a property of date assigned to the current date from JavaScript's Date object
+
+  access this prop in the CurrentDate component - showing its value within the p tags.
 */
