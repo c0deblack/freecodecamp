c347f20d83663586913b15ac15642a899c3d1371
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-12 18:30:55 -0400

    Write a react component from scratch.

--- front-end-development-libraries/react/write-a-react-component-from-scratch.jsx
+++ front-end-development-libraries/react/write-a-react-component-from-scratch.jsx
@@ -36,7 +36,15 @@ class MyComponent extends React.Component {
 
 ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
 /*
-Define a class MyComponent that extends React.Component. Its render method should return a div that contains an h1 tag with the text: My First React Component! in it. Use this text exactly, the case and punctuation matter. Make sure to call the constructor for your component, too.
+Define a class MyComponent that extends React.Component.
 
-Render this component to the DOM using ReactDOM.render(). There is a div with id='challenge-node' available for you to use.
+Its render method should return a div that contains an h1 tag with the text:
+
+  My First React Component!
+  
+Make sure to call the constructor for your component, too.
+
+Render this component to the DOM using ReactDOM.render().
+
+There is a div with id='challenge-node' available for you to use.
 */
