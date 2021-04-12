8d4eadd3b5875ac7fe99e8f65d218e22d1a199cd
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-12 18:30:55 -0400

    Render state in the user interface another way.

--- front-end-development-libraries/react/render-state-in-the-user-interface-another-way.jsx
+++ front-end-development-libraries/react/render-state-in-the-user-interface-another-way.jsx
@@ -21,7 +21,9 @@ class MyComponent extends React.Component {
     }
   };
 /*
-In the MyComponent render method, define a const called name and set it equal to the name value in the component's state. Because you can write JavaScript directly in this part of the code, you don't have to enclose this reference in curly braces.
+In the MyComponent render method, define a const called name.
 
-Next, in the return statement, render this value in an h1 tag using the variable name. Remember, you need to use the JSX syntax (curly braces for JavaScript) in the return statement.
+Set it equal to the name value in the component's state.
+
+Render this value in an h1 tag using the variable name within the return statement.
 */
