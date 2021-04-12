dc71154ac9344c030768fab4d2b7a0a10a38c683
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-12 18:30:54 -0400

    Override default props.

--- front-end-development-libraries/react/override-default-props.jsx
+++ front-end-development-libraries/react/override-default-props.jsx
@@ -5,7 +5,7 @@ Explicitly set the prop values for a component.
 
 If quantity is an integer, it should be wrapped in curly braces.
 
-{100}
+  {100}
 */
 const Items = (props) => {
     return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
@@ -26,5 +26,9 @@ const Items = (props) => {
     }
   };
 /*
-The ShoppingCart component now renders a child component Items. This Items component has a default prop quantity set to the integer 0. Override the default prop by passing in a value of 10 for quantity.
+The ShoppingCart component now renders a child component Items.
+
+This Items component has a default prop quantity set to the integer 0.
+
+Override the default prop by passing in a value of 10 for quantity.
 */
