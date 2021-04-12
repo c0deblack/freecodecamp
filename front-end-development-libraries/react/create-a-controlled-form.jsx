1a2963574ca02e76f27b2ce1efbf83b2e2501c60
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-12 18:30:54 -0400

    Create a controlled form.

--- front-end-development-libraries/react/create-a-controlled-form.jsx
+++ front-end-development-libraries/react/create-a-controlled-form.jsx
@@ -38,9 +38,13 @@ class MyForm extends React.Component {
   }
 }
 /*
-  We've added a button which submits the form. You can see it has the type set to submit indicating it is the button controlling the form. Add the input element in the form and set its value and onChange() attributes like the last challenge. You should then complete the handleSubmit method so that it sets the component state property submit to the current input value in the local state.
+  Add the input element in the form and set its value and onChange() attributes like the last challenge.
   
-  Note: You also must call event.preventDefault() in the submit handler, to prevent the default form submit behavior which will refresh the web page.
+  Complete the handleSubmit method.
   
-  Finally, create an h1 tag after the form which renders the submit value from the component's state. You can then type in the form and click the button (or press enter), and you should see your input rendered to the page.
+  Set the component state property submit to the current input value in the local state.
+  
+  Prevent the default form submit behavior by calling event.preventDefault() in the submit handler.
+
+  Create an h1 tag after the form which renders the submit value from the component's state.
   */
