d1f887ac7ac0ee59751060a542b9cd5f78fb12b8
c0deblack <79178809+c0deblack@users.noreply.github.com>
2021-04-22 20:59:37 -0400

    Define an action creator.

---
+++ front-end-development-libraries/redux/define-an-action-creator.jsx
@@ -0,0 +1,13 @@
+/*
+Define an Action Creator
+
+A JavaScript function that returns an action.
+*/
+const action = {
+  type: 'LOGIN'
+}
+
+const actionCreator = () => action;
+/*
+Define a function named actionCreator() that returns the action object when called.
+*/
